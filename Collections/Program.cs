969643c609f1a4e44470ceeab042218a0c8998a7
Esma Kızılkaya <38670768+esmakzlkaya@users.noreply.github.com>
2021-01-20 22:55:59 +0300

    oop1,collections

---
+++ Collections/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //array
+            // new dediğimiz anda bellekte yeni bir adres oluşur.!!
+
+            //string[] isimler = new string[] {"Engin","Murat","Kerem","Halil"};
+            //Console.WriteLine(isimler[0]);
+
+            //List  .. genelde listeleri kullanırız
+
+            List<string> isimler2 = new List<string> { "Engin","Murat", "Kerem", "Halil"};
+            Console.WriteLine(isimler2[0]);
+            isimler2.Add("İlker");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+            
+
+        }
+    }
+}
---
+++ OOP1/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class Product
+    {
+        public int Id { get; set; } //primarykey
+        public int CategoryId { get; set; } //foreignkey
+        public string ProductName { get; set; }
+        public double UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+        
+        //CRUD-Create-Read-Update-Delete
+
+
+    }
+}
